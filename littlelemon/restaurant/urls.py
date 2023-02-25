57691a2939bf340fc158d59310daef40c964e8fc
voimkoko <avbfugml@gmail.com>
2023-02-25 18:51:38 -0500

    menu-booking-api

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('menu/', views.MenuItemView.as_view(), name = "menu"),
+    path('menu/<int:pk>', views.SingleMenuView.as_view()),
 ]
